34ecb6b670dc63dc8b1ea9849985c5aad699c926
donFrenzel <drf118@scarletmail.rutgers.edu>
2025-11-25 16:12:32 -0500

    Update fileGenerator.c
    Updated it so that it does all of the file pooling stuff as an array of strings.

--- fileGenerator.c
+++ fileGenerator.c
@@ -4,7 +4,6 @@
 
 #define MAX_THREADS 4
 
-
 char *generateFiles(const char *file_prefix, const char *file_suffix, int file_num)
 {
     //get the size of the total file string including the prefix and the suffix.  
@@ -22,6 +21,7 @@ char *generateFiles(const char *file_prefix, const char *file_suffix, int file_n
     strcat(fileName,file_suffix);
 
     //Assign the null pointer at the end of it.  
+    fileName[totalLenFileName]='\0';
     //printf("%i\n",totalLenFileName);
     return fileName;
     free(fileName);
@@ -29,17 +29,32 @@ char *generateFiles(const char *file_prefix, const char *file_suffix, int file_n
 
 int main()
 {
-    //Main function shows how to open it
+    //Main function shows how to open it and demonstrates how file names can be generated.  In the same vein, 
+    // a loop can be made which closes the files which contain that particular indentifying number, also in the range of 0 and 
+    // MAX_THREADS -1, using strcmp to determine that it ought to be closed from the file pool, as once the program has completed
+    // everything ought to be closed.  
     
     const char *prefix = "bungity_ungity";
     const char *suffix = ".txt";
     char *fileName;
-    //fileName = generateFiles(prefix,suffix,MAX_THREADS);
 
+    //Create the file pool, allowing 20 characters per.  Array of strings which can then be opened and closed at the user's
+    //leisure.  
+    char filePool[MAX_THREADS][20];
 
+    //For loop which creates a fileName from the given i and then copies it into its repsective position in the file pool.  
     for(int i=0;i<MAX_THREADS;i++)
     {
+        //Create all file names, then construct the filePool
         fileName = generateFiles(prefix,suffix,i);
-        printf("%s\n",fileName);
+        //printf("%s\n",fileName);
+        strcpy(filePool[i],fileName);
     }
+
+    //Sees what's in the file pool, using an array of strings.  Mostly for proof of concept.  
+    for(int j=0;j<MAX_THREADS;j++)
+    {
+        printf("%s\n",filePool[j]);
+    }
+
 }
